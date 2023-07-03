f5ab812d49f251294bb2d51f78b312ba4b940682
sanghyeonLee <95309008+sanghyeonLee2@users.noreply.github.com>
2023-07-03 21:53:30 +0900

    feat : 클라이언트 DB에 책 추가 기능 작성중

--- src/component/InsertBook.js
+++ src/component/InsertBook.js
@@ -1,32 +1,29 @@
 import React from "react";
-import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 function InsertBook() {
-  const navigate = useNavigate();
-  const [bookTitle, setBookTitle] = useState("");
-  const [bookPrice, setBookPrice] = useState("");
-  const [bookPublish, setBookPublish] = useState("");
-  const [bookWriter, setBookWriter] = useState("");
+  //const [book, setBook] = useState("");
   //const [bookPicture, setBookPicture] = useState("");
   const onSubmitBook = (e) => {
     e.preventDefault();
-    const book = {
-      bookTitle,
-      bookPrice,
-      bookPublish,
-      bookWriter,
+    const bookInfo = {
+      bookTitle: e.target.childNodes[0].value,
+      bookPrice: e.target.childNodes[1].value,
+      bookPublish: e.target.childNodes[2].value,
+      bookWriter: e.target.childNodes[3].value,
     };
+    console.log(bookInfo);
+
     axios
-      .post("http://localhost:3001/insertBook", book)
+      .post("http://localhost:3001/insertBook", bookInfo)
       .then((res) => {
         if (res.data === false) {
           console.log(res.data);
           alert("이미 책이 있습니다.");
+          return;
         } else {
           console.log(res.data);
           alert("책 등록이 완료되었습니다.");
-          navigate("/books", { state: { book } });
         }
       })
       .catch((err) => console.log(err));
@@ -35,30 +32,10 @@ function InsertBook() {
   return (
     <div>
       <form onSubmit={onSubmitBook}>
-        <input
-          type="text"
-          name="title"
-          placeholder="책 제목"
-          onChange={(e) => setBookTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          name="title"
-          placeholder="가격"
-          onChange={(e) => setBookPrice(e.target.value)}
-        />
-        <input
-          type="text"
-          name="title"
-          placeholder="출판사"
-          onChange={(e) => setBookPublish(e.target.value)}
-        />
-        <input
-          type="text"
-          name="title"
-          placeholder="작가"
-          onChange={(e) => setBookWriter(e.target.value)}
-        />
+        <input type="text" name="title" placeholder="책 제목" />
+        <input type="text" name="title" placeholder="가격" />
+        <input type="text" name="title" placeholder="출판사" />
+        <input type="text" name="title" placeholder="작가" />
         <input type="submit" />
       </form>
     </div>
